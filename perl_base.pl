6e20a313af982169fbc69f758cf447326406ce10
Sofia Kiseleva <s.kiseleva@corp.mail.ru>
2014-10-27 13:19:53 -0400

    recursion parser in perl_base.pl

--- perl_base.pl
+++ perl_base.pl
@@ -3,6 +3,7 @@
 use Net::Server::Fork;
 use JSON;
 use Data::Dumper;
+use IO::File;
 use strict;
 #=begin
 
@@ -99,4 +100,164 @@ print "123";
 
     print "TEST end\n";
 ### TESTTTTTTTTTTTTTTT
+
+
+sub do_parse {
+    my $fh = shift; #указатель на файл
+    my $it = shift; # ссылка на предыдущую структуру
+    my $is_ar = shift; # тип предыдущей структуры - хэш или массив
+    my $brackets = shift; #ссылка на массив со скобками
+
+    print "FUNCTION do_parse,".Dumper($it)."\n";
+
+    my $prev_line;
+    my $is_first = 1;
+
+    while (<$fh>) {
+        chomp $_;
+        print "line $_\n";
+        print "\$prev_line: $prev_line, \$is_first: $is_first\n";
+        s/\s*//;
+
+        if (/^\{\}$/) {
+            next;
+        }
+        if (/^\[\]$/) {
+            next;
+        }
+        if (/^\{$/) {
+            push @{$brackets}, '}'; #type of expected closing bracket
+            print "brackets: ".Dumper(@{$brackets})."\n";
+            %{$it->{$prev_line}} = ();
+            print "it {} before: ".Dumper($it)."\n";
+            if (do_parse($fh, \%{$it->{$prev_line}}, 0, $brackets) <= 0) {
+                return -1;
+            }
+            print "it {} after: ".Dumper($it)."\n";
+            $is_first = 1;
+            next;
+        }
+        if (/^\[$/) {
+            push @{$brackets}, ']';
+            print "brackets: ".Dumper(@{$brackets})."\n";
+            @{$it->{$prev_line}} = ();
+            print "it [] before: ".Dumper($it)."\n";
+            if (do_parse($fh, \@{$it->{$prev_line}}, 1, $brackets) <= 0) {
+                return -1;
+            }
+            print "it {} after: ".Dumper($it)."\n";
+            $is_first = 1;
+            next;
+        } 
+        if (/^\]/) {
+            my $last = pop @{$brackets};
+            unless ($last eq ']') {
+                print "123 Syntax error in config, check ] brackets, ".Dumper(@{$brackets})."\n";
+                return -1;
+            }
+            push @{$it}, $prev_line;
+            return 1;
+        }
+        if (/^\}/) {
+            my $last = pop @{$brackets};
+            unless ($last eq '}') {
+                print "456 Syntax error in config, check } brackets, ".Dumper(@{$brackets})."\n";
+                return -1;
+            }
+            if ($is_first) {
+                return 1;
+            } else {
+                print "789 Syntax error in config, check {} or [] brackets, ".Dumper(@{$brackets})."\n";
+                return -1;
+            }
+            next;
+        }
+
+        if (/^$/) {next;}
+        if (/^\#/) {next;}
+
+        if ($is_first) {
+            $prev_line = $_;
+            $is_first = 0;
+
+
+            print "LINE is $_\n";
+            if (/^(\w+)[:]?\s*[\(\[]\s*([\s\,\w\(\)]+)[\]\)]\,*\s*$/) {
+                my $key = $1;
+                my $value = $2;
+                my @ar = split (/, */, $value);
+                @{$it->{$key}} = @ar;
+                print "it {} after little push: ".Dumper($it)."\n";
+                $is_first = 1;
+            }
+            elsif (/^(\w+)\s*\:\"?\s*(\w+)\s*\"?\s*\,?$/) {
+                $it->{$1} = $2;
+                print "it {} after little push: ".Dumper($it)."\n";
+                $is_first = 1;
+            }
+
+            ### TODO обработать первый элемент, все равно массива или хэша вида a:1, при этом $is_first снова станет равным 1
+        } else {
+            if ($is_ar) {
+                push @{$it}, $prev_line;
+                $prev_line = $_;
+            } else {
+                print "000 Syntax error in config, check {} or [] brackets, ".Dumper(@{$brackets})."\n"; 
+                return -1;
+            }
+        }
+    }
+    return 1;
+}
+
+
+sub parse_test_file1 {
+    print "FUNCTION parse_test_file1\n";
+    my $test_file = shift;
+
+    my $fh = new IO::File;
+    unless ($fh->open("$test_file")) {
+        die "file($test_file) with imap tests not found\n";
+    }
+
+    my @brackets;
+    my %glhash;
+    my $is_new_bracket = 0;
+    my $is_init_item = 1;
+
+    if (do_parse($fh, \%glhash, 0, \@brackets) <= 0) {
+        print "ERROR in parsing\n";
+        $fh->close();
+        return -1;
+    } else {
+        print "OK, parsing completed\n";
+    }
+
+    $fh->close();
+    print Dumper(%glhash);
+}
+
+
+#parse_test_file("tests/test3");
+parse_test_file1("tests/test2");
+
+
+sub func2 {
+    my $fh = shift; 
+    while( <$fh> ) { 
+        #print "a line: $_";
+    } 
+} 
+
+sub func1 { 
+    my $fh = new IO::File; 
+    print "func1\n";
+    unless ($fh->open("tests/test2")) { 
+          die "file not found\n"; 
+    } 
+    func2($fh); 
+    $fh->close(); 
+}
+
+func1();
 1;
